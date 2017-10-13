b93d7adde1ca058b9c62b66e2c2887b04f4fec35
igorlivio <igor.livio@gmail.com>
2017-10-13 16:20:24 -0300

    tabulação editada

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 int main() {
   int a[5], modsum = 0, i2, mod0, mod1, mod2, mod3, mod4;
   //float execucao[4], bc2; // para realizar os testes
-  char c;
   int n;
 
 
@@ -12,32 +11,33 @@ int main() {
   for (int i=0; i<n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
 
- 	i2 = i + 1;
-  	mod0 = i2 % a[0];
+    i2 = i + 1; //pode ser retirado, mas preferi nao mexer na estrutura já pronta
+    mod0 = i2 % a[0];
     mod1 = i2 % a[1];
     mod2 = i2 % a[2];
     mod3 = i2 % a[3];
     mod4 = i2 % a[4];
-    //printf("mod 0..4: %d %d %d %d %d i2: %i i1: %i\n", mod0, mod1, mod2, mod3, mod4, i2, i);
-    if (mod0 == 0) printf("%d", 0);
+    //printf("mod 0..4: %d %d %d %d %d i2: %i i1: %i\n", mod0, mod1, mod2, mod3, mod4, i2, i); //for debugging
+    if (mod0 == 0) printf("%d", 0); // no printf poder ser substituido por uma respectiva string sem alteração no resultado
     else modsum++;
     if (mod1 == 0) printf("%d", 1);
-	else modsum++;
+    else modsum++;
     if (mod2 == 0) printf("%d", 2);
-	else modsum++;
+    else modsum++;
     if (mod3 == 0) printf("%d", 3);
-	else modsum++;
+    else modsum++;
     if (mod4 == 0) printf("%d", 4);
-	else modsum++;
+    else modsum++;
 
-	if( modsum == 5) printf("-\n");
-	else printf("\n");
+    if( modsum == 5) printf("-\n");
+    else printf("\n");
 
-	modsum = 0;
+    modsum = 0;
 
     
-/*  //for debugging  
-  	printf("entrou: %i\n", i2); 
+/*  
+    //for debugging  
+    printf("entrou: %i\n", i2); 
     bc2 = i2%3; //bc será igual a 1
     if (bc2 == 0.0) printf("topzera\n");
     printf("%f %i\n", bc2, i2); 
@@ -48,8 +48,8 @@ int main() {
 	// You can track that using gdb, it allows you to see what is going on inside your C program while it runs.
 	// For more details : http://www.cs.swarthmore.edu/~newhall/unixhelp/howto_gdb.html
 
-  	printf("i2: %d\n", i2);
-  	execucao[0] = i2 % a[0];
+    printf("i2: %d\n", i2);
+    execucao[0] = i2 % a[0];
     execucao[1] = i2 % a[1];
     execucao[2] = i2 % a[2];
     execucao[3] = i2 % a[3];
